f15bbc07e6e04fd9328a9c0298249b67a4aa5b39
omeIets <maria.leticiasbarboza@gmail.com>
2025-01-22 00:12:04 -0300

    linha de chegada funcional e funcao audios

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from sys import exit  # pesquisar função dessa biblioteca !!!
 
 # funcao gerar pergunta e resposta
 
+def tocar_audios():
+    '''
+    essa função auxilia no gerenciamento dos diferentes audios do jogo a serem tocados
+    '''
+    if audio_tocando == 1:
+        pygame.mixer.music.load('audios/audio_senna.mp3')
+    if audio_tocando == 2:
+        pygame.mixer.music.load('audios/musica_tema.mp3')
+    pygame.mixer.music.play()
+
 pygame.init()  # inicia o pygame (ajuda a renderizar imagens, tocar sons, etc)
 pygame.display.set_caption('Math Racing')
 # pygame.display.set_icon()
 screen = pygame.display.set_mode((900, 600))
 screen.fill((44, 43, 43, 1))
 
-# surfaces e rects
+# surfaces e rects ////////////////////////////////////////////////
 
 # tela inicial
 carro_ferrari = pygame.image.load('images/carro_ferrari.png').convert_alpha()
@@ -65,6 +75,8 @@ txt_voltar_rect = txt_voltar.get_rect(center=(70, 40))
 # text = ''
 
 test_font = pygame.font.Font(None, 50)
+audio_tocando = 2
+# tocar_audios()
 
 game_mode = vencedor = tempo_inicial = mexer = 0
 cenarios = int(round((900 / tam_cenario) + 1, 0))
@@ -73,7 +85,7 @@ clock = pygame.time.Clock()
 
 # onde o jogo acontece
 while True:
-
+    
     # checar todos os eventos
     for event in pygame.event.get():  # o metodo capta os eventos e o loop passa por todos eles
         if event.type == pygame.QUIT:  # o mesmo que clicar no botão de x
@@ -145,57 +157,77 @@ while True:
                     game_mode = 0
 
     if game_mode == 1:  # jogo em si
+        fundo = pygame.Surface((900, 600))
+        fundo.fill((44, 43, 43, 1))
+        tempo_atual = pygame.time.get_ticks() # variaveis de tempo so existem durante game mode 1!!
 
-        if not fim:
-            fundo = pygame.Surface((900, 600))
-            fundo.fill((44, 43, 43, 1))
-            tempo_atual = pygame.time.get_ticks() # variaveis de tempo so existem durante game mode 1!!
-
-            # cronometro dos rounds
-            timer_round = 10 - ((tempo_atual - tempo_inicial) / 1000)
-            if timer_round <= 0:
-                if player == 1:
-                    player = 2
-                else:
-                    player = 1
-                # reseta tempos pois vai mudar round
-                tempo_inicial = tempo_atual = pygame.time.get_ticks()
+        # cronometro dos rounds
+        timer_round = 10 - ((tempo_atual - tempo_inicial) / 1000)
+        if timer_round <= 0: # quando zera gira os turnos
+            if player == 1:
+                player = 2
+            else:
+                player = 1
+            # reseta tempos pois vai mudar round
+            tempo_inicial = tempo_atual = pygame.time.get_ticks()
             
-            # rounds
-            if player == 1:
-                screen.blit(fundo_player1, (0, 0))
-                screen.blit(seta_esquerda, seta_esquerda_rect)
-            else:
-                screen.blit(fundo_player2, (0, 0))
-                screen.blit(seta_direita, seta_direita_rect)
-        
-
-             # --------------temporario--------------
-            timer_surf = test_font.render( f'{timer_round:.2f}', False, 'White')  
-            if player == 1:
-                score_rect = timer_surf.get_rect(center=(214, 105))
-            else:
-                score_rect = timer_surf.get_rect(center=(687, 105))
-            # --------------------------------------
-
-            screen.blit(pista,(0,0))
-            screen.blit(carro_azul, carro_azul_rect)
-            screen.blit(carro_vermelho, carro_vermelho_rect)
-            screen.blit(timer_surf, score_rect)
-
-            #cenario se mexendo
-            for i in range(0, cenarios):
-                screen.blit(cenario, (i * tam_cenario + mexer, 0))
-
-            #variavel mexer
-            mexer -= 5
-
-            #resetando a imagem quando chega na borda
-            if abs(mexer) > tam_cenario:
-                mexer = 0
-
+        # rounds
+        if player == 1:
+            screen.blit(fundo_player1, (0, 0))
+            screen.blit(seta_esquerda, seta_esquerda_rect)
+            # chama funcao contas
+            # blit contas e etc
         else:
-            game_mode = 2
+            screen.blit(fundo_player2, (0, 0))
+            screen.blit(seta_direita, seta_direita_rect)
+            # chama funcao contas
+            # blit contas e etc
+
+        # ---------------------temporario----------------------
+        timer_surf = test_font.render( f'{timer_round:.2f}', False, 'White')  
+        if player == 1:
+            score_rect = timer_surf.get_rect(center=(214, 105))
+        else:
+            score_rect = timer_surf.get_rect(center=(687, 105))
+        # -----------------------------------------------------
+
+        screen.blit(pista,(0,0))
+        screen.blit(carro_azul, carro_azul_rect)
+        screen.blit(carro_vermelho, carro_vermelho_rect)
+        screen.blit(timer_surf, score_rect)
+            
+
+        #cenario se mexendo
+        for i in range(0, cenarios):
+            screen.blit(cenario, (i * tam_cenario + mexer, 0))
+        #variavel mexer
+        mexer -= 5
+        #resetando a imagem quando chega na borda
+        if abs(mexer) > tam_cenario:
+            mexer = 0
+
+        # carros passando pela linha de chegada
+        if carro_vermelho_rect.right > 840: # se um dos carros passa o x da linha de chegada
+            if audio_tocando != 1: # toca audio so uma vez
+                audio_tocando = 1
+                tocar_audios()
+            screen.blit(chegada,(840,395)) # aparece a linha de chegada
+            screen.blit(carro_vermelho, carro_vermelho_rect) # blit dnv para nao ficar por baixo da linha de chegada
+            carro_vermelho_rect.left += 3 # vai andando o carro
+            if carro_vermelho_rect.left > 900: # quando a bunda do carro passar dos 900 da tela muda o game mode
+                game_mode = 0
+                vencedor = 1
+        elif carro_azul_rect.right > 840:
+            if audio_tocando != 1:
+                audio_tocando = 1
+                tocar_audios()
+            screen.blit(chegada,(840,395))
+            screen.blit(carro_azul, carro_azul_rect)
+            carro_azul_rect.left += 3
+            if carro_azul_rect.left > 900:
+                game_mode = 0
+                vencedor = 2
+
 
         # RENDERIZANDO O INPUT
         # Render the current text.
@@ -221,14 +253,12 @@ while True:
         fundo.fill((44, 43, 43, 1))
         screen.blit(fundo, (0, 0))
 
-        if fim:
             # mostrar carro vencedor e tela jogar de novo
-            if vencedor == 1:
-                screen.blit(carro_ferrari, carro_ferrari_rect)
-            else:
-                screen.blit(carro_williams, carro_ferrari_rect)
+        if vencedor == 1:
+            screen.blit(carro_ferrari, carro_ferrari_rect)
+        elif vencedor == 2:
+            screen.blit(carro_williams, carro_ferrari_rect)
                 # screen.blit(carro_williams, carro_ferrari_rect)
-                #
         else:
             screen.blit(txt_logo, txt_logo_rect)
             screen.blit(xadrez, (0, 0))
